d8c2c5bf9762e492c07a7cf962593d4e4f13538e
ALGOMARBLER\algom <YW30520@gmail.com>
2024-05-11 19:25:25 +1200

    changed button

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myfirstapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        changeText(R.color.blurple);
+        changeButton(R.color.blurple, R.string.button_text);
     }
 
     private void changeText(int colorId) {
@@ -18,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
         textview.setText(R.string.hi);
         textview.setTextColor(getColor(colorId));
 
+    }
+
+    private void changeButton(int colorId, int text) {
+        Button button = findViewById(R.id.button0001);
+        button.setOnClickListener(v->{
+            button.setText(text);
+            button.setTextColor(getColor(colorId));
+        });
     }
 }
 
