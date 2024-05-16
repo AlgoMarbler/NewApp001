a617d7af7b64753d1b7171d269206e041d395542
ALGOMARBLER\algom <YW30520@gmail.com>
2024-05-16 22:38:39 +1200

    made the app into a clicker game

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,36 +1,53 @@
 package com.example.myfirstapp;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    int goldAmount = 0;
+    int increment = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        changeButton(R.color.blurple, R.string.button_text);
+        Button button2 = findViewById(R.id.gainButton);
+        Button buttonUpgrade = findViewById(R.id.upgrade);
+        Button buttonUpgrade2 = findViewById(R.id.upgrade2);
+        Button buttonUpgrade3 = findViewById(R.id.upgrade3);
+        Button buttonUpgrade4 = findViewById(R.id.upgrade4);
+        Button buttonUpgrade5 = findViewById(R.id.upgrade5);
+        button2.setOnClickListener(v -> changeAmount());
+
+        buttonUpgrade.setOnClickListener(v -> changeIncrement(50, 1));
+        buttonUpgrade2.setOnClickListener(v -> changeIncrement(400, 5));
+        buttonUpgrade3.setOnClickListener(v -> changeIncrement(1250, 12));
+        buttonUpgrade4.setOnClickListener(v -> changeIncrement(7000, 55));
+        buttonUpgrade5.setOnClickListener(v -> changeIncrement(35000, 220));
     }
 
-    private void changeText(int colorId) {
-        TextView textview = findViewById(R.id.centerTextView);
-        textview.setText(R.string.hi);
-        textview.setTextColor(getColor(colorId));
 
+    private void changeIncrement(int price,int amountAdded) {
+        if(goldAmount >= price) {
+            increment += amountAdded;
+            goldAmount -= price;
+            updateStats();
+        }
     }
 
-    private void changeButton(int colorId, int text) {
-        Button button = findViewById(R.id.button0001);
-        button.setOnClickListener(v->{
-            button.setText(text);
-            button.setTextColor(getColor(colorId));
-        });
+    private void changeAmount() {
+        goldAmount += increment;
+        updateStats();
     }
+
+    private void updateStats() {
+        TextView incrementTextView = findViewById(R.id.incrementDisplay);
+        incrementTextView.setText("Per click: " + increment);
+        TextView textView = findViewById(R.id.goldDisplay);
+        textView.setText(String.valueOf(goldAmount));
+    }
+
 }
-
-
-
-
